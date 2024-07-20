29dd697fbc1afd315aba6fb02b2e55ddc165a314
jean <jeanbarrosd@outlook.com>
2024-07-20 13:30:47 -0300

    Adicionado Function de alteracao de local e adicionar companhia

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import {MapPin, Calendar, ArrowRight} from "lucide-react";
+import {MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2} from "lucide-react";//importando icones do lucide-react
+import { useState } from "react";
 
 
 function App() {
- 
+  
+  //o input esta aberto? "false" significa nao, entao faça...
+  //a resposta da mudança de estado vai ser passado pelo setIsInputOPen
+  const [isInputOpen, setIsInputOPen] = useState(false);
+
+  function InputOpen(){
+    setIsInputOPen(true); // agora ele esta aberto e essa resposta vai refletir no isInputOpen
+  };
+
+  function closeInput(){
+    setIsInputOPen(false); // agora ele esta fechado e essa resposta vai refletir no isInputOpen
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-backg">
      
@@ -13,31 +26,66 @@ function App() {
           <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
         </div>
         
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-            
-            <div className=" flex items-center gap-2 flex-1">
-              <MapPin  className=" size-5 text-zinc-400"/>
-              <input type="text" placeholder="Para onde gostaria de ir?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
-            </div>
+        <div className="space-y-4">
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+              
+              <div className=" flex items-center gap-2 flex-1">
+                <MapPin  className=" size-5 text-zinc-400"/>
+                {/*disabled={isInputOpen}, butao desabilitado caso o input esteja open */}
+                <input disabled={isInputOpen} type="text" placeholder="Para onde gostaria de ir?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+              </div>
 
 
-            <div className=" flex items-center gap-2">
-              <Calendar  className=" size-5 text-zinc-400"/>
-              <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
-            
-            </div>
+              <div className=" flex items-center gap-2">
+                <Calendar  className=" size-5 text-zinc-400"/>
+                <input disabled={isInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+              
+              </div>
 
               <div className="w-px h-6 bg-zinc-800"/>
 
-            <button className="bg-blue-400 text-blue-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-blue-300 " >
-              Continuar
-              <ArrowRight className="size-5 text-blue-950"/>
-            </button>
+
+              {isInputOpen ? (
+                <button onClick={closeInput} className="bg-blue-400 text-blue-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-blue-300 ">
+                  Alterar local/data
+                  <Settings2 /> 
+                </button>
+               
+                ) : (
+                    /*onClick={InputOpen} significa que quando o butao é apertado vai fazer  */
+                    <button onClick={InputOpen} className="bg-blue-400 text-blue-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-blue-300 " >
+                      Continuar
+                      <ArrowRight className="size-5 text-blue-950"/>
+                    </button>
             
-        </div>
-        
+                  )
+              } 
+            
+          </div>
+
+          {/*isInputOpen ? "aberto" : "fechado"} = mostra que acontece quando clico no butao apos colocar */}
+            {isInputOpen && (
+              <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+                
+                <div className="flex items-center gap-2 flex-1">
+                  <UserRoundPlus  className=" size-5 text-zinc-400"/>
+                  <input type="text" placeholder="Terá companhia nesta viagem?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+                </div>
+
+                <div className="w-px h-6 bg-zinc-800"/>
+
+                  {/*onClick={InputOpen} significa que quando o butao é apertado vai fazer  */}
+                <button className="bg-blue-400 text-blue-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-blue-300 " >
+                  Confirmar viagem
+                  <ArrowRight className="size-5 text-blue-950"/>
+                </button>
+                
+              </div>
+            )}
+        </div> 
+
         <p className="text-sm text-zinc-400">
-          Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+          Ao planejar sua viagem pela Trip Navegator você automaticamente concorda <br />
           com nossos <a className="text-zinc-300 underline" href="">termos de uso</a> e <a className="text-zinc-300 underline" href="">políticas de privacidade.</a></p>
       </div>
     </div>
